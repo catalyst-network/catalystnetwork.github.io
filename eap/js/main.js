6549492fb03b01231c2d9696e1811efcd9717029
stephenhorsfall <stephen.horsfall@atlascity.io>
2019-09-19 13:23:11 +0100

    add email signup

--- eap/js/main.js
+++ eap/js/main.js
@@ -2,7 +2,7 @@ var template = {
     title: 'JOIN THE FIRST DLT NETWORK WHICH ENABLES ALL DEVELOPERS TO PARTICIPATE IN THE DECENTRALISED COMPUTING REVOLUTION!',
     description: 'The Catalyst Network is a fast, light, secure and decentralised network that enables all developers to quickly develop and deploy dapps and smart contracts — without needing to invest time and money learning new programming languages. Use your existing skills to tap into a rapidly expanding market and join a growing community of developers working to bring blockchain technology into the mainstream.',
     socialCard: '',
-    url: 'https:/catalystnet.org',
+    url: 'https:/catalystnet.org/eap',
     twitter: '@CatalystNetOrg',
     metaTitle: 'Catalyst Developer Signup',
     metaDescription: '',
